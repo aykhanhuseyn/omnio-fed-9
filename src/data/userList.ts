1a25352fd1850e89481d6bdba78f330fd5d905af
aykhanhuseyn <immense_ideas@icloud.com>
2023-02-26 16:51:05 +0400

    fix: message icons

--- src/data/userList.ts
+++ src/data/userList.ts
@@ -1,4 +1,4 @@
-import Jeff from '../../public/profile/jeff.jpeg'
+import Jeff from '../../public/profile/jeff.jpeg';
 
 export enum Messenger {
 	WHATSAPP = 'Whatsapp',
@@ -8,6 +8,7 @@ export enum Messenger {
 	LINKEDIN = 'Linkedin',
 	TELEGRAM = 'Telegram',
 	SNAPCHAT = 'Snapchat',
+	EMAIL = 'Email',
 }
 
 export const messengerData = [
@@ -21,139 +22,148 @@ export const messengerData = [
 		id: 2,
 		color: '#3B5998',
 		name: Messenger.FACEBOOK,
-		icon: '/profile/Facebook.png',
+		icon: '/profile/Vector.png',
+	},
+	{
+		id: 3,
+		color: '#3498DB',
+		name: Messenger.EMAIL,
+		icon: '/profile/VectorMail.png',
 	},
 ];
 
+export type ContactListUser = {
+	id: number;
+	name: string;
+	messenger: Messenger;
+	profilePic: string;
+	lastText: string;
+	profileText: string;
+	lastTextTime: string;
+};
 
-export const contactList = [
-    {
-      id: 1,
-      name: "William",
-      profilePic: "/profile/william.png",
-      iconPic: '/profile/Vector.png',
-      lastText: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit, accusamus ",
-      profileText: 'Westworld',
-      lastTextTime: "01.01.2050",
-    },
-    {
-      id: 2,
-      name: "Emil@grace.com",
-      profilePic: "/profile/emily grace.png",
-      iconPic: '/profile/VectorMail.png',
-      profileText: 'Westworld',
-      lastText: `Lorem Ipsum`,
-      lastTextTime: "01.01.2050",
-    },
-    {
-        id: 3,
-        name: "+11417543010",
-        profilePic: "/profile/logan.png",
-        iconPic: '/profile/VectorWp.png',
-        profileText: 'Westworld',
-        lastText: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit, accusamus?",
-        lastTextTime: "01.01.2050",
-      },
-    {
-      id: 4,
-      name: "caleb@nicholas.com",
-      profilePic: "/profile/caleb nichols.png",
-      iconPic: '/profile/VectorMail.png',
-      profileText: 'Westworld',
-      lastText: "Lorem Ipsum",
-      lastTextTime: "01.01.2050",
-    },
-    {
-      id: 5,
-      name: "+12417543010",
-      profilePic: "/profile/photo none.jpg",
-      iconPic: '/profile/VectorWp.png',
-      profileText: 'Westworld',
-      lastText: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit, accusamus?",
-      lastTextTime: "01.01.2050",
-    },
-    {
-        id: 6,
-        name: "Man in Black",
-        profilePic: "/profile/man in black.png",
-        iconPic: '/profile/Vector.png',
-        profileText: 'Westworld',
-        lastText: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit, accusamus?",
-        lastTextTime: "01.01.2050",
-      }, 
-      {
-        id: 7,
-        name: "charlotte@hale.com",
-        profilePic: "/profile/charlotte hale.png",
-        iconPic: '/profile/VectorMail.png',
-        profileText: 'Westworld',
-        lastText: "Lorem Ipsum",
-        lastTextTime: "01.01.2050",
-      },
-      {
-        id: 8,
-        name: "Lucy Sanders",
-        profilePic: "/profile/photo none.jpg",
-        iconPic: '/profile/Vector.png',
-        profileText: 'Westworld',
-        lastText: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit, accusamus?",
-        lastTextTime: "01.01.2050",
-      },
-  ];
+export const contactList: ContactListUser[] = [
+	{
+		id: 1,
+		name: 'William',
+		messenger: Messenger.WHATSAPP,
+		profilePic: '/profile/william.png',
+		lastText:
+			'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit, accusamus ',
+		profileText: 'Westworld',
+		lastTextTime: '01.01.2050',
+	},
+	{
+		id: 2,
+		name: 'Emil@grace.com',
+		profilePic: '/profile/emily grace.png',
+		messenger: Messenger.FACEBOOK,
+		profileText: 'Westworld',
+		lastText: `Lorem Ipsum`,
+		lastTextTime: '01.01.2050',
+	},
+	{
+		id: 3,
+		name: '+11417543010',
+		profilePic: '/profile/logan.png',
+		messenger: Messenger.EMAIL,
+		profileText: 'Westworld',
+		lastText:
+			'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit, accusamus?',
+		lastTextTime: '01.01.2050',
+	},
+	{
+		id: 4,
+		name: 'caleb@nicholas.com',
+		profilePic: '/profile/caleb nichols.png',
+		messenger: Messenger.WHATSAPP,
+		profileText: 'Westworld',
+		lastText: 'Lorem Ipsum',
+		lastTextTime: '01.01.2050',
+	},
+	{
+		id: 5,
+		name: '+12417543010',
+		profilePic: '/profile/photo none.jpg',
+		messenger: Messenger.WHATSAPP,
+		profileText: 'Westworld',
+		lastText:
+			'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit, accusamus?',
+		lastTextTime: '01.01.2050',
+	},
+	{
+		id: 6,
+		name: 'Man in Black',
+		profilePic: '/profile/man in black.png',
+		messenger: Messenger.WHATSAPP,
+		profileText: 'Westworld',
+		lastText:
+			'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit, accusamus?',
+		lastTextTime: '01.01.2050',
+	},
+	{
+		id: 7,
+		name: 'charlotte@hale.com',
+		profilePic: '/profile/charlotte hale.png',
+		messenger: Messenger.WHATSAPP,
+		profileText: 'Westworld',
+		lastText: 'Lorem Ipsum',
+		lastTextTime: '01.01.2050',
+	},
+	{
+		id: 8,
+		name: 'Lucy Sanders',
+		profilePic: '/profile/photo none.jpg',
+		messenger: Messenger.WHATSAPP,
+		profileText: 'Westworld',
+		lastText:
+			'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit, accusamus?',
+		lastTextTime: '01.01.2050',
+	},
+];
 
-  export const messagesList = [
-    {
-      id: 1,
-      messageType: "TEXT",
-      text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit, accusamus 😁",
-      senderID: 0,
-      addedOn: "12:00 PM",
-    
-      
-    },
-    {
-      id: 2,
-      messageType: "TEXT",
-      text: "Lorem ipsum dolor ",
-      senderID: 1,
-      addedOn: "12:01 PM",
-
-
-    },
-    {
-      id: 3,
-      messageType: "TEXT",
-      text: "Lorem ipsum dolor sit amet consectetur",
-      senderID: 0,
-      addedOn: "12:02 PM",
-     
-
-    },
-    {
-      id: 4,
-      messageType: "TEXT",
-      text: "Lorem ipsum dolor sit amet consectetur adipisicing",
-      senderID: 1,
-      addedOn: "12:03 PM",
-  
-
-    },
-    {
-      id: 5,
-      messageType: "TEXT",
-      text: "Lorem ipsum dolor sit amet consectetur adipisicing 😁",
-      senderID: 0,
-      addedOn: "12:10 PM",
-
-
-    },
-    {
-      id: 5,
-      messageType: "TEXT",
-      text: "Lorem ipsum dolor sit amet consectetur ?",
-      senderID: 1,
-      addedOn: "12:11 PM",
-
-    },
-  ];
-  
+export const messagesList = [
+	{
+		id: 1,
+		messageType: 'TEXT',
+		text:
+			'Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit, accusamus 😁',
+		senderID: 0,
+		addedOn: '12:00 PM',
+	},
+	{
+		id: 2,
+		messageType: 'TEXT',
+		text: 'Lorem ipsum dolor ',
+		senderID: 1,
+		addedOn: '12:01 PM',
+	},
+	{
+		id: 3,
+		messageType: 'TEXT',
+		text: 'Lorem ipsum dolor sit amet consectetur',
+		senderID: 0,
+		addedOn: '12:02 PM',
+	},
+	{
+		id: 4,
+		messageType: 'TEXT',
+		text: 'Lorem ipsum dolor sit amet consectetur adipisicing',
+		senderID: 1,
+		addedOn: '12:03 PM',
+	},
+	{
+		id: 5,
+		messageType: 'TEXT',
+		text: 'Lorem ipsum dolor sit amet consectetur adipisicing 😁',
+		senderID: 0,
+		addedOn: '12:10 PM',
+	},
+	{
+		id: 5,
+		messageType: 'TEXT',
+		text: 'Lorem ipsum dolor sit amet consectetur ?',
+		senderID: 1,
+		addedOn: '12:11 PM',
+	},
+];
--- src/pages/Mbox/Conversation.tsx
+++ src/pages/Mbox/Conversation.tsx
@@ -1,365 +1,386 @@
-import { useState } from "react";
-import styled from "styled-components";
-import CheckOutlinedIcon from "@mui/icons-material/CheckOutlined";
-import SearchIcon from "@mui/icons-material/Search";
+import { useState } from 'react';
+import styled from 'styled-components';
+import CheckOutlinedIcon from '@mui/icons-material/CheckOutlined';
+import SearchIcon from '@mui/icons-material/Search';
 import CloseIcon from '@mui/icons-material/Close';
-import { messagesList } from "../../data/userList";
-import MenuItem from "@mui/material/MenuItem";
-import FormControl from "@mui/material/FormControl";
-import Select, { SelectChangeEvent } from "@mui/material/Select";
-import Picker from 'emoji-picker-react'
+import {
+	ContactListUser,
+	messagesList,
+	messengerData,
+} from '../../data/userList';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+import Picker from 'emoji-picker-react';
+import { type } from 'os';
+import { find } from 'lodash';
 
-
+// components/shared/Container.tsx
+// components/MBox/Container.tsx
 const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  height: 100vh;
-  flex: 3;
-  background: #f7f8fd;
+	display: flex;
+	flex-direction: column;
+	height: 100vh;
+	flex: 3;
+	background: #f7f8fd;
 `;
 
+// styled-components
+// type
+// styled
+
+// components/MBox/YourComponent/*
+// Component
+// styles
+// index.ts
+// YourComponent.tsx
+// YourComponentHelperComponent1.tsx
+// YourComponentHelperComponent2.tsx
+// YourComponentHelperComponent3.tsx
+
+// components/MBox/ProfileHeader/*
+// index.ts
+// ProfileHeader.tsx
+// ContactInfo.styled.tsx
+// ProfileImage.styled.tsx
+// ProfileName.styled.tsx
 const ProfileHeader = styled.div`
-  display: flex;
-  align-items: center;
-  border-bottom: 1px solid lightgray;
+	display: flex;
+	align-items: center;
+	border-bottom: 1px solid lightgray;
 `;
 
 const ContactInfo = styled.div`
-  flex: 0.9;
-  gap: 10px;
+	flex: 0.9;
+	gap: 10px;
 `;
 
 const ProfileImage = styled.img`
-  width: 45px;
-  height: 45px;
-  border-radius: 50px;
-  margin: 15px;
+	width: 45px;
+	height: 45px;
+	border-radius: 50px;
+	margin: 15px;
 `;
 
 const ProfileName = styled.span`
-  font-size: 15px;
-  font-weight: bold;
-  margin: 10px 5px;
+	font-size: 15px;
+	font-weight: bold;
+	margin: 10px 5px;
 `;
 
 const SearchContainer = styled.div`
-  position: relative;
-  width: ${(props) => (props.isSearching ? 360 : 48)}px;
-  height: 48px;
-  background-color: #f2f2f2;
-  border-radius: 26px;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  padding-left: 5px;
-  transition: 0.3s all ease;
-  margin-left: 20px;
+	position: relative;
+	width: ${(props) => (props.isSearching ? 360 : 48)}px;
+	height: 48px;
+	background-color: #f2f2f2;
+	border-radius: 26px;
+	display: flex;
+	align-items: center;
+	justify-content: space-between;
+	padding-left: 5px;
+	transition: 0.3s all ease;
+	margin-left: 20px;
 `;
 
 const SearchInput = styled.input`
-  padding-left: 48px;
-  border: none;
-  outline: none;
-  position: absolute;
-  top: 0;
-  left: 0;
-  height: 100%;
-  width: 100%;
-  background-color: transparent;
-  font-size: 16px;
-  border: 1px solid transparent;
+	padding-left: 48px;
+	border: none;
+	outline: none;
+	position: absolute;
+	top: 0;
+	left: 0;
+	height: 100%;
+	width: 100%;
+	background-color: transparent;
+	font-size: 16px;
+	border: 1px solid transparent;
 
-  &:focus {
-    border-color: rgba(0, 0, 0, 0.3);
-    border-radius: 26px;
-  }
+	&:focus {
+		border-color: rgba(0, 0, 0, 0.3);
+		border-radius: 26px;
+	}
 `;
 
 const IconButton = styled.button`
-  position: relative;
-  width: 36px;
-  height: 36px;
-  border: none;
-  z-index: 1;
-  cursor: pointer;
-  background: none;
+	position: relative;
+	width: 36px;
+	height: 36px;
+	border: none;
+	z-index: 1;
+	cursor: pointer;
+	background: none;
 
-  &:hover {
-    opacity: 0.4;
-    &::after {
-      opacity: 1;
-      transform: scale(1);
-    }
-  }
+	&:hover {
+		opacity: 0.4;
+		&::after {
+			opacity: 1;
+			transform: scale(1);
+		}
+	}
 
-  &::after {
-    content: "";
-    position: absolute;
-    top: 0;
-    left: 0;
-    height: 100%;
-    border-radius: 50%;
-    z-index: -1;
-    background-color: #000;
-    transition: 0.2s ease;
-    transform: scale(0.6);
-    opacity: 0;
-  }
+	&::after {
+		content: '';
+		position: absolute;
+		top: 0;
+		left: 0;
+		height: 100%;
+		border-radius: 50%;
+		z-index: -1;
+		background-color: #000;
+		transition: 0.2s ease;
+		transform: scale(0.6);
+		opacity: 0;
+	}
 `;
 
 const HeaderIconDiv = styled.div`
-  display: flex;
-  align-items: center;
-  margin-top: 5px;
+	display: flex;
+	align-items: center;
+	margin-top: 5px;
 `;
 
-const HeaderIcon = styled.img`
-  border-radius: 50%;
-  width: 15px;
-  height: 15px;
-  background-color: #3b5998;
-  border: 3px solid #3b5998;
+type HeaderIconProps = {
+	bg: string;
+};
+const HeaderIcon = styled.img<HeaderIconProps>`
+	border-radius: 50%;
+	width: 15px;
+	height: 15px;
+	background-color: ${(props) => props.bg};
+	object-fit: contain;
+	border: 1px solid #fff;
 `;
 
 const IconName = styled.span`
-  color: #3b5998;
-  font-family: "Roboto";
-  font-weight: 500;
-  font-size: 12px;
+	color: #3b5998;
+	font-family: 'Roboto';
+	font-weight: 500;
+	font-size: 12px;
 `;
 
 const RightSide = styled.div`
-  display: flex;
-  align-items: center;
-  gap: 20px;
+	display: flex;
+	align-items: center;
+	gap: 20px;
 `;
 
 const BgIcon = styled.div`
-  padding: 7px;
-  background: #f2f4ff;
-  border-radius: 50%;
+	padding: 7px;
+	background: #f2f4ff;
+	border-radius: 50%;
 `;
 
 const ProfilePhoto = styled.img`
-  width: 30px;
-  height: 30px;
-  border-radius: 50%;
+	width: 30px;
+	height: 30px;
+	border-radius: 50%;
 `;
 
-
 const ChatBox = styled.div`
-  display: flex;
-  background-color: #fff;
-  padding: 10px;
-  align-items: center;
-  bottom: 0;
+	display: flex;
+	background-color: #fff;
+	padding: 10px;
+	align-items: center;
+	bottom: 0;
 `;
 
 const MessageInputContainer = styled.div`
-  display: flex;
-  align-items: center;
-  flex-direction: row;
-  background-color: #f5f5f5;
-  border-radius: 28px;
-  width: 1000px;
-  height: 56px;
-  margin-left: 10px;
+	display: flex;
+	align-items: center;
+	flex-direction: row;
+	background-color: #f5f5f5;
+	border-radius: 28px;
+	width: 1000px;
+	height: 56px;
+	margin-left: 10px;
 `;
 
 const MessageInput = styled.input`
-  width: 100%;
-  background-color: #f5f5f5;
-  border: none;
-  outline: none;
-  font-size: 15px;
-  margin-left: 10px;
+	width: 100%;
+	background-color: #f5f5f5;
+	border: none;
+	outline: none;
+	font-size: 15px;
+	margin-left: 10px;
 `;
 
 const AttachImage = styled.img`
-  width: 24px;
-  height: 13px;
-  cursor: pointer;
+	width: 24px;
+	height: 13px;
+	cursor: pointer;
 `;
 
 const AttachInput = styled.input`
-  display: none;
+	display: none;
 `;
 
 const SendImage = styled.img`
-  margin-left: 10px;
-  cursor: pointer;
+	margin-left: 10px;
+	cursor: pointer;
 `;
 
 const MessageContainer = styled.div`
-  display: flex;
-  flex-direction: column;
-  height: 100%;
-  background: #FAFAFA;
-  overflow-y: auto;
-
+	display: flex;
+	flex-direction: column;
+	height: 100%;
+	background: #fafafa;
+	overflow-y: auto;
 `;
 
 const MessageDiv = styled.div<{ isYours: boolean }>`
-  display: flex;
-  justify-content: ${(props) => (props.isYours ? "flex-end" : "flex-start")};
-  margin: 5px 15px;
+	display: flex;
+	justify-content: ${(props) => (props.isYours ? 'flex-end' : 'flex-start')};
+	margin: 5px 15px;
 `;
 
 const Message = styled.div<{ isYours: boolean }>`
-  background: ${(props) => (props.isYours ? "#574B90" : "#EEE3F4")};
-  max-width: 50%;
-  color: #303030;
-  padding: 35px 16px;
-  font-size: 14px;
-  border-radius: 12px 12px 0px 12px;
-  color: ${(props) => (props.isYours ? "#fff" : "#000")};
+	background: ${(props) => (props.isYours ? '#574B90' : '#EEE3F4')};
+	max-width: 50%;
+	color: #303030;
+	padding: 35px 16px;
+	font-size: 14px;
+	border-radius: 12px 12px 0px 12px;
+	color: ${(props) => (props.isYours ? '#fff' : '#000')};
 `;
 
 const MessageTime = styled.p`
-    margin-top: 60px;
-    font-size: 12px;
-    color: gray;
-  
-`
+	margin-top: 60px;
+	font-size: 12px;
+	color: gray;
+`;
 
+function Conversation(props: { selectChat: ContactListUser }) {
+	const [user, setUser] = useState('');
 
-function Conversation(props: any) {
-  const [user, setUser] = useState("");
+	const handleChange = (event: SelectChangeEvent) => {
+		setUser(event.target.value);
+	};
 
-  const handleChange = (event: SelectChangeEvent) => {
-    setUser(event.target.value);
-  };
+	const [isActive, setIsActive] = useState(false);
+	const toggleSearch = () => {
+		setIsActive(!isActive);
+	};
 
-  const [isActive, setIsActive] = useState(false);
-  const toggleSearch = () => {
-    setIsActive(!isActive);
-  };
+	const { selectChat } = props;
+	const [text, setText] = useState('');
+	const [messageList, setMessageList] = useState(messagesList);
+	// const onEmojiClick = (event, emoji) => {}
+	const onEnterPress = (event: any) => {
+		if (event.key === 'Enter') {
+			const messages = [...messageList];
+			messages.push({
+				id: 0,
+				messageType: 'TEXT',
+				text,
+				senderID: 0,
+				addedOn: '12.02 PM',
+			});
+			setMessageList(messages);
+			setText('');
+		}
+	};
 
-  const { selectChat } = props;
-  const [text, setText] = useState('');
-  const [messageList, setMessageList] = useState(messagesList);
-  // const onEmojiClick = (event, emoji) => {}
-  const onEnterPress = (event: any) => {
-    if (event.key === "Enter") {
-      const messages = [...messageList];
-      messages.push({
-        id: 0,
-        messageType: "TEXT",
-        text,
-        senderID: 0,
-        addedOn: "12.02 PM",
-        
-      });
-      setMessageList(messages);
-      setText('');
-    }
-  };
+	return (
+		<Container>
+			<ProfileHeader>
+				<ProfileImage src={selectChat.profilePic} />
+				<ContactInfo>
+					<ProfileName>{selectChat.name}</ProfileName>
 
-  return (
-    <Container>
-      <ProfileHeader>
-        <ProfileImage src={selectChat.profilePic} />
-        <ContactInfo>
-          <ProfileName>{selectChat.name}</ProfileName>
+					<HeaderIconDiv>
+						<HeaderIcon
+							bg={find(messengerData, { name: selectChat.messenger })?.color!}
+							src={find(messengerData, { name: selectChat.messenger })?.icon!}
+						/>
+						<IconName>{selectChat.profileText}</IconName>
+					</HeaderIconDiv>
+				</ContactInfo>
 
-          <HeaderIconDiv>
-        
-            <HeaderIcon src={selectChat.iconPic} />
-            <IconName>{selectChat.profileText}</IconName>
-          </HeaderIconDiv>
-        </ContactInfo>
+				<RightSide>
+					<FormControl sx={{ minWidth: 250, border: 'none' }} size='small'>
+						<Select
+							style={{
+								borderRadius: '25px',
+							}}
+							value={user}
+							onChange={handleChange}
+						>
+							<MenuItem value='Dolores'>
+								<ProfilePhoto src='profile/dolores abernathy.png' />
+								<ProfileName
+									style={{
+										fontWeight: '200',
+									}}
+								>
+									Dolores Abernathy
+								</ProfileName>
+							</MenuItem>
 
-        <RightSide>
-          <FormControl  sx={{ minWidth: 250, border: "none", }} size="small">
-            <Select
-              style={{
-                borderRadius: "25px",
-              }}
-              
-              value={user}
-    
-              onChange={handleChange}
-            >
-          
-              <MenuItem value="Dolores">
-                <ProfilePhoto src="profile/dolores abernathy.png" />
-                <ProfileName
-                  style={{
-                    fontWeight: "200"
-                  }}>
-                  Dolores Abernathy
-                </ProfileName>
-              </MenuItem>
-             
+							<MenuItem value='Meave'>
+								<ProfilePhoto src='/profile/photo none.jpg' />
+								<ProfileName
+									style={{
+										fontWeight: '200',
+									}}
+								>
+									Meave Millay
+								</ProfileName>
+							</MenuItem>
+							<MenuItem value='Teddy'>
+								<ProfilePhoto src='/profile/photo none.jpg' />
+								<ProfileName
+									style={{
+										fontWeight: '200',
+									}}
+								>
+									Teddy Flood
+								</ProfileName>
+							</MenuItem>
+						</Select>
+					</FormControl>
 
-              <MenuItem value="Meave">
-                <ProfilePhoto src="/profile/photo none.jpg" />
-                <ProfileName
-                  style={{
-                    fontWeight: "200",
-                  }}
-                >
-                  Meave Millay
-                </ProfileName>
-              </MenuItem>
-              <MenuItem value="Teddy">
-              <ProfilePhoto src="/profile/photo none.jpg" />
-                <ProfileName
-                  style={{
-                    fontWeight: "200",
-                  }}
-                >
-                  Teddy Flood
-                </ProfileName>
-              </MenuItem>
-            </Select>
-          </FormControl>
+					<BgIcon>
+						<IconButton>
+							<CheckOutlinedIcon />
+						</IconButton>
+					</BgIcon>
+					<SearchContainer isSearching={isActive}>
+						<IconButton onClick={toggleSearch}>
+							{isActive ? <CloseIcon /> : <SearchIcon />}
+						</IconButton>
+						<SearchInput type='text' placeholder='Search' />
+					</SearchContainer>
+				</RightSide>
+			</ProfileHeader>
 
-          <BgIcon>
-          <IconButton>
-            <CheckOutlinedIcon />
-          </IconButton>
-          </BgIcon>
-          <SearchContainer isSearching={isActive}>
-            <IconButton onClick={toggleSearch}>
-              {isActive ? <CloseIcon /> : <SearchIcon />}
-            </IconButton>
-            <SearchInput type="text" placeholder="Search" />
-          </SearchContainer>
-        </RightSide>
-      </ProfileHeader>
+			<MessageContainer>
+				{messageList.map((messageData) => (
+					<MessageDiv isYours={messageData.senderID === 0}>
+						<Message isYours={messageData.senderID === 0}>{messageData.text}</Message>
+						<MessageTime>{messageData.addedOn}</MessageTime>
+					</MessageDiv>
+				))}
+			</MessageContainer>
+			<ChatBox>
+				<AttachInput type='file' id='file' />
+				<label htmlFor='file'>
+					<AttachImage src={'/profile/Attach.png'} />
+				</label>
 
-      <MessageContainer>
-        {messageList.map((messageData) => (
-          <MessageDiv isYours={messageData.senderID === 0}>
-            <Message isYours={messageData.senderID === 0}>
-              {messageData.text}
-              
-            </Message>
-            <MessageTime>{messageData.addedOn}</MessageTime>
-          </MessageDiv>
-        ))}
-      </MessageContainer>
-      <ChatBox>
-        <AttachInput type="file" id="file" />
-        <label htmlFor="file">
-          <AttachImage src={"/profile/Attach.png"} />
-        </label>
+				<MessageInputContainer>
+					{/* <Picker onEmojiClick={onEmojiClick} /> */}
 
-        <MessageInputContainer>
-          {/* <Picker onEmojiClick={onEmojiClick} /> */}
-
-          <MessageInput
-            placeholder="Type your message"
-            value={text}
-            onKeyDown={onEnterPress}
-            onChange={(e) => setText(e.target.value)}
-          />
-        </MessageInputContainer>
-        <SendImage src="/profile/Send.png" />
-      </ChatBox>
-    </Container>
-  );
+					<MessageInput
+						placeholder='Type your message'
+						value={text}
+						onKeyDown={onEnterPress}
+						onChange={(e) => setText(e.target.value)}
+					/>
+				</MessageInputContainer>
+				<SendImage src='/profile/Send.png' />
+			</ChatBox>
+		</Container>
+	);
 }
 
 export default Conversation;
